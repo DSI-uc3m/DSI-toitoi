a978cfa239d05c0fb4e48848e916acda81b3c46f
cmhernandez-uc3m <100346186@alumnos.uc3m.es>
2018-04-05 12:46:01 +0200

    Modificaciones v7

--- practica02/src/app/app.component.ts
+++ practica02/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
 import { EstadoCoche } from './coche';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   title = 'Minipráctica 02';
   
   public EstadoCoche:any = EstadoCoche;
+  public filtro;
   
   public coches:Array<Coche>=[new Coche('dacia', 'sandero', new Date(2007, 10, 1), new Date (2018, 3, 1), 5000, 'sandero.jpg', EstadoCoche.BUENO),
   new Coche('opel', 'corsa', new Date(2002, 11, 14), new Date (2018, 3, 6), 1200, '', EstadoCoche.MALO),
