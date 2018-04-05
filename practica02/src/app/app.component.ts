ba1c736f507c437b834e3667f713a7458121e5b9
cmhernandez-uc3m <100346186@alumnos.uc3m.es>
2018-04-05 13:16:22 +0200

    Modificaciones v8

--- practica02/src/app/app.component.ts
+++ practica02/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
 import { EstadoCoche } from './coche';
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -10,16 +10,27 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class AppComponent {
+
+
   title = 'Minipráctica 02';
   
   public EstadoCoche:any = EstadoCoche;
   public filtro;
   
-  public coches:Array<Coche>=[new Coche('dacia', 'sandero', new Date(2007, 10, 1), new Date (2018, 3, 1), 5000, 'sandero.jpg', EstadoCoche.BUENO),
-  new Coche('opel', 'corsa', new Date(2002, 11, 14), new Date (2018, 3, 6), 1200, '', EstadoCoche.MALO),
-  new Coche('dacia', 'logan', new Date(2005, 2, 14), new Date (2018, 3, 1), 3340, '', EstadoCoche.MALO),
-  new Coche('citroen', 'xsara picasso', new Date(2001, 1, 30), new Date (2018, 1, 3), 2350, '', EstadoCoche.BUENO)]
+  public coches:Array<Coche>=[new Coche('dacia', 'sandero', new Date(2007, 10, 1), new Date (2018, 3, 1), 5000, 'sandero.jpg', EstadoCoche.BUENO, 0),
+  new Coche('opel', 'corsa', new Date(2002, 11, 14), new Date (2018, 3, 6), 1200, '', EstadoCoche.MALO, 1),
+  new Coche('dacia', 'logan', new Date(2005, 2, 14), new Date (2018, 3, 1), 3340, '', EstadoCoche.MALO, 2),
+  new Coche('citroen', 'xsara picasso', new Date(2001, 1, 30), new Date (2018, 1, 3), 2350, '', EstadoCoche.BUENO, 3)]
   
+  public filtrados = [];
+  
+  
+  ngOnInit(){
+  	for(let i = 0; i < this.coches.length; i++)
+  	this.filtrados.push(this.coches[i]);
+  	}
+  
+  //TODO: Solucionar el tema de los índices
   public rebajar(i){
 	this.coches[i].precio = this.coches[i].precio*0.9;
 }
@@ -27,6 +38,25 @@ export class AppComponent {
   public vendido(i){
 	this.coches.splice(i, 1);
 }
+
+  //TODO: Solucionar que se muestren los que EMPIEZAN por ese substring en lugar de los que lo contienen
+  public getFiltrados(){
+  
+  	if (this.filtro == null) return this.filtrados;
+  
+  	for(let x = 0; x < this.coches.length; x++){
+  		if(this.coches[x].marca.includes(this.filtro) && this.filtrados.indexOf(this.coches[x]) == -1){
+  			this.filtrados.push(this.coches[x]);
+  		}
+  		
+  		if(!this.coches[x].marca.includes(this.filtro) && this.filtrados.indexOf(this.coches[x]) != -1){
+  			this.filtrados.splice(this.filtrados.indexOf(this.coches[x]));
+  		}
+  		
+  		
+  	}
+  	return this.filtrados;
+  }
 }
 
 
--- practica02/src/app/coche.ts
+++ practica02/src/app/coche.ts
@@ -9,16 +9,15 @@ public fecha_modelo:Date;
 public fecha_venta:Date;
 public precio:number;
 public estado:EstadoCoche;
+public id:number;
 
 public getPVP(){
 	return this.precio*1.21;
 }
 
-public rebajar(){
-	this.precio = this.precio*0.9;
-}
 
-constructor(marca1, modelo1, fecha_modelo1, fecha_venta1, precio1, foto1, estado1){
+
+constructor(marca1, modelo1, fecha_modelo1, fecha_venta1, precio1, foto1, estado1, id1){
 this.marca = marca1;
 this.modelo = modelo1;
 this.foto = foto1;
@@ -26,6 +25,7 @@ this.fecha_modelo = fecha_modelo1;
 this.fecha_venta = fecha_venta1;
 this.precio = precio1;
 this.estado = estado1;
+this.id = id1;
 }
 
 }
