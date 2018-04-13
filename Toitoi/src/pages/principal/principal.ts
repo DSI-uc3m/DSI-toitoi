031c9bdcb852e7367a896c37a33e848b61996978
cmhernandez-uc3m <100346186@alumnos.uc3m.es>
2018-04-13 16:31:41 +0200

    Paso de pantallas vdefinitiva

--- Toitoi/src/pages/principal/principal.ts
+++ Toitoi/src/pages/principal/principal.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 /**
- * Generated class for the HomeUsuarioPage page.
+ * Generated class for the PrincipalPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,19 +10,23 @@ import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
-  selector: 'page-home-usuario',
-  templateUrl: 'home-usuario.html',
+  selector: 'page-principal',
+  templateUrl: 'principal.html',
 })
-export class HomeUsuarioPage {
+export class PrincipalPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomeUsuarioPage');
+    console.log('ionViewDidLoad PrincipalPage');
   }
-  
-  irPagSiguiente(){
+
+  irArtista(){
+  	this.navCtrl.push('SubirObraPage');
+  	}
+  	
+  irUsuario(){
   	this.navCtrl.push('VerObraPage');
   	}
   	
